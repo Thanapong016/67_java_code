c02fbbd19d38e8aa3940a1bf927b0711b63a5ac9
Thanapong016 <674230016@webmail.npru.ac.th>
2024-09-02 12:44:57 +0700

    Add files via upload

---
+++ W12/W12_01_WhileTrue.java
@@ -0,0 +1,22 @@
+package W12;
+
+public class W12_01_WhileTrue {
+    public static void main(String[] args) {
+
+    int i = 0;                              // กำหนดค่าเริ่มต้น
+    while (true) {                          
+        i++;                                // เพี่มค่า i ทีละ 1
+        System.out.print(i+" ");           // แสดงค่า i
+        
+        if(i >= 1000){
+            break;
+        }
+        
+        // if(i >=1000) break; //เขียนลดรูป if ให้สั้นลง
+    
+    
+        // ตรวจสอบเงื่อนไข ถ้ารอบมากกว่าหรือเท่ากับ 1000 ให้ออกจากลูป
+        // ออกจากลูป
+        }
+    }
+}
